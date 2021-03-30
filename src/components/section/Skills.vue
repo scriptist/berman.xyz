0b09c578bc7cdfe8bb5d233d1c0e979cce3c3c6c
Mike Berman <mike@berman.xyz>
2021-03-30 19:42:26 +0100

    Update skills

--- src/components/section/Skills.vue
+++ src/components/section/Skills.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="skills">
     <div class="col">
-      <h2>Languages</h2>
       <ul>
+        <li>Frameworks</li>
+        <ul>
+          <li>React</li>
+          <li>Vue.js</li>
+          <li>Webpack</li>
+        </ul>
         <li>JavaScript</li>
         <ul>
           <li>ES6</li>
@@ -10,26 +15,19 @@
           <li>Flow</li>
           <li>Node.js</li>
         </ul>
-        <li>CSS</li>
-        <ul>
-          <li>SASS</li>
-          <li>LESS</li>
-          <li>PostCSS</li>
-        </ul>
-        <li>HTML5</li>
-        <li>PHP</li>
-        <li>Python</li>
       </ul>
     </div>
 
     <div class="col">
-    <h2>Libraries</h2>
       <ul>
-        <li>React</li>
-        <li>Vue.js</li>
-        <li>AngularJS</li>
-        <li>Webpack</li>
-        <li>Gulp</li>
+        <li>CSS</li>
+        <ul>
+          <li>SASS</li>
+          <li>PostCSS</li>
+        </ul>
+        <li>HTML5</li>
+        <li>PHP &amp; Hack</li>
+        <li>Python</li>
       </ul>
     </div>
   </div>

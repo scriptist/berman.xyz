6b7842283e01251955e1014132f51949d33400ea
Mike Berman <mike@berman.xyz>
2025-11-03 13:10:50 -0600

    Added tbody

--- src/components/section/Contact.vue
+++ src/components/section/Contact.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="contact">
     <table>
-      <tr>
-        <th>Email:</th>
-        <td><a href="mailto:mike@berman.xyz">mike@berman.xyz</a></td>
-      </tr>
-      <tr>
-        <th>LinkedIn:</th>
-        <td>
-          <a href="https://www.linkedin.com/in/mikeyberman/" target="_blank"
-            >linkedin.com/in/mikeyberman</a
-          >
-        </td>
-      </tr>
-      <tr>
-        <th>GitHub:</th>
-        <td><a href="https://github.com/scriptist" target="_blank">github.com/scriptist</a></td>
-      </tr>
-      <tr>
-        <th>Location:</th>
-        <td>Sydney, Australia</td>
-      </tr>
+      <tbody>
+        <tr>
+          <th>Email:</th>
+          <td><a href="mailto:mike@berman.xyz">mike@berman.xyz</a></td>
+        </tr>
+        <tr>
+          <th>LinkedIn:</th>
+          <td>
+            <a href="https://www.linkedin.com/in/mikeyberman/" target="_blank"
+              >linkedin.com/in/mikeyberman</a
+            >
+          </td>
+        </tr>
+        <tr>
+          <th>GitHub:</th>
+          <td><a href="https://github.com/scriptist" target="_blank">github.com/scriptist</a></td>
+        </tr>
+        <tr>
+          <th>Location:</th>
+          <td>Sydney, Australia</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
